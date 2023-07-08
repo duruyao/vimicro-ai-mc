cf9faf1a6e16c4d8129fa7f93ca470394ca2a42c
duruyao <duruyao@gmail.com>
2023-07-08 11:06:49 +0800

    chore: update the name of the target image

--- docker/build.sh
+++ docker/build.sh
@@ -4,4 +4,4 @@ set -euxo pipefail
 
 docker buildx create --use --name insecure-builder --buildkitd-flags '--allow-insecure-entitlement security.insecure' || true
 
-docker buildx build --allow security.insecure --platform linux/arm64/v8 -t vimicro-ai-mc:cpu -f Dockerfile.cpu_arm .
+docker buildx build --allow security.insecure --platform linux/arm64/v8 -t vimicro-ai-mc:768_cpu -f Dockerfile.cpu_arm .
