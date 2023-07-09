b67079d72799746e1c6f8c790bf2ca3407bbf489
duruyao <duruyao@gmail.com>
2023-07-09 01:01:13 +0000

    build: run commands in the background with nohup

--- docker/build.sh
+++ docker/build.sh
@@ -4,4 +4,7 @@ set -euxo pipefail
 
 docker buildx create --use --name insecure-builder --buildkitd-flags '--allow-insecure-entitlement security.insecure' || true
 
-docker buildx build --allow security.insecure --platform linux/arm64/v8 -t vimicro-ai-mc:768_cpu -f Dockerfile.cpu_arm .
+nohup docker buildx build --allow security.insecure --platform linux/arm64/v8 -t vimicro-ai-mc:768_cpu -f Dockerfile.cpu_arm . --progress plain > nohup.out 2>&1 &
+
+tail -f nohup.out
+
