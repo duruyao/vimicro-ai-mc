482ec2ce4daf0128539fa7f7249c6263ad3c4e2b
duruyao <duruyao@gmail.com>
2023-07-10 10:58:59 +0800

    chore: load docker image from cache to local

--- docker/build.sh
+++ docker/build.sh
@@ -4,7 +4,7 @@ set -euxo pipefail
 
 docker buildx create --use --name insecure-builder --buildkitd-flags '--allow-insecure-entitlement security.insecure' || true
 
-nohup docker buildx build --allow security.insecure --platform linux/arm64/v8 -t vimicro-ai-mc:768_cpu -f Dockerfile.cpu_arm . --progress plain > nohup.out 2>&1 &
+nohup docker buildx build --allow security.insecure --platform linux/arm64/v8 -t duruyao/vimicro-ai-mc:768_cpu -f Dockerfile.cpu_arm --load . --progress plain > nohup.out 2>&1 &
 
 tail -f nohup.out
 
