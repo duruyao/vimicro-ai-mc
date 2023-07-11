eb6d7417adc2f7ff6a988275f3c23b6d12da3268
duruyao <duruyao@gmail.com>
2023-07-11 13:26:40 +0800

    build: build by running the command '. build.sh'

--- docker/build.sh
+++ docker/build.sh
@@ -1,10 +1,6 @@
-#!/usr/bin/env bash
-
-set -euxo pipefail
 
 docker buildx create --use --name insecure-builder --buildkitd-flags '--allow-insecure-entitlement security.insecure' || true
 
 nohup docker buildx build --allow security.insecure --platform linux/arm64/v8 -t duruyao/vimicro-ai-mc:768_cpu -f Dockerfile.cpu_arm --load . --progress plain > nohup.out 2>&1 &
 
 tail -f nohup.out
-
