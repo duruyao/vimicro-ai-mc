15be3eb412b64caba0e72020eaa579c4a98278aa
duruyao <duruyao@gmail.com>
2023-07-12 16:05:39 +0800

    chore: install an earlier version of pytorch

--- docker/install/ubuntu_install_torch.sh
+++ docker/install/ubuntu_install_torch.sh
@@ -7,6 +7,6 @@ set -o pipefail
 pip3 install future --no-cache-dir
 
 pip3 install --no-cache-dir \
-  torch==1.12.0 \
-  torchvision==0.13.0 \
+  torch==1.10.2 \
+  torchvision==0.11.3 \
   --extra-index-url https://download.pytorch.org/whl/cpu
