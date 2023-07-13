b62a3f3307ee33183835860db9e9420d771e1eb1
duruyao <duruyao@gmail.com>
2023-07-13 12:07:00 +0800

    style: use ANSI color codes

--- docker/utils/help.sh
+++ docker/utils/help.sh
@@ -4,28 +4,63 @@ set -e
 set -u
 set -o pipefail
 
-cat <<EOF
+BLACK="\033[0;30m"
+RED="\033[0;31m"
+GREEN="\033[0;32m"
+YELLOW="\033[0;33m"
+BLUE="\033[0;34m"
+MAGENTA="\033[0;35m"
+CYAN="\033[0;36m"
+WHITE="\033[0;37m"
 
+BG_BLACK="\033[0;40m"
+BG_RED="\033[0;41m"
+BG_GREEN="\033[0;42m"
+BG_YELLOW="\033[0;43m"
+BG_BLUE="\033[0;44m"
+BG_MAGENTA="\033[0;45m"
+BG_CYAN="\033[0;46m"
+BG_WHITE="\033[0;47m"
+
+BOLD_BLACK="\033[1;30m"
+BOLD_RED="\033[1;31m"
+BOLD_GREEN="\033[1;32m"
+BOLD_YELLOW="\033[1;33m"
+BOLD_BLUE="\033[1;34m"
+BOLD_MAGENTA="\033[1;35m"
+BOLD_CYAN="\033[1;36m"
+BOLD_WHITE="\033[1;37m"
+
+NC="\033[0m"
+BOLD="\033[1m"
+NBOLD="${NC}"
+CMD="\033[7m  "
+NCMD="  ${NC}"
+
+echo -e "${BOLD}
  __      __  _____   __  __   _____    _____   _____     ____                        _____            __  __    _____
  \ \    / / |_   _| |  \/  | |_   _|  / ____| |  __ \   / __ \               /\     |_   _|          |  \/  |  / ____|
   \ \  / /    | |   | \  / |   | |   | |      | |__) | | |  | |  ______     /  \      | |    ______  | \  / | | |
    \ \/ /     | |   | |\/| |   | |   | |      |  _  /  | |  | | |______|   / /\ \     | |   |______| | |\/| | | |
     \  /     _| |_  | |  | |  _| |_  | |____  | | \ \  | |__| |           / ____ \   _| |_           | |  | | | |____
-     \/     |_____| |_|  |_| |_____|  \_____| |_|  \_\  \____/           /_/    \_\ |_____|          |_|  |_|  \_____|
+     \/     |_____| |_|  |_| |_____|  \_____| |_|  \_\  \____/           /_/    \_\ |_____|          |_|  |_|  \_____|${NBOLD}
 
-0) Move to a workspace with complete code and data.
-  cd /path/to/your_workspace
+${BOLD}
+1) Launch the docker image${NBOLD}
+  Move to a workspace with complete code and data:
+  ${CMD}cd /path/to/your_workspace${NCMD}
 
-1) Launch the docker image and use the built-in model compilation tools.
-  docker run -it --name \$USER.mc.$((100 + RANDOM % 900)) --rm -w \$PWD -v \$(realpath \$PWD):\$PWD duruyao/vimicro-ai-mc:768_cpu bash
+  Launch the docker image and use the built-in model compilation tools:
+  ${CMD}docker run -it --rm -w \$PWD -v \$(realpath \$PWD):\$PWD duruyao/vimicro-ai-mc:768_cpu bash${NCMD}
 
-2) Launch the docker image and install the latest model compilation tools (NOT REQUIRED).
-  docker run -it --name \$USER.mc.$((100 + RANDOM % 900)) --rm -w \$PWD -v \$(realpath \$PWD):\$PWD -v /path/to/mc_release:/opt/mc duruyao/vimicro-ai-mc:768_cpu bash
-
-3) See how to convert other AI models to Caffe models.
-  pto2caffe --help
-
-4) See how to convert Caffe models to NPU models.
-  caffe2npu --help
-
-EOF
+  Launch the docker image and install the latest model compilation tools (${BOLD}NOT REQUIRED${NBOLD}):
+  ${CMD}docker run -it --rm -w \$PWD -v \$(realpath \$PWD):\$PWD -v /path/to/mc_release:/opt/mc duruyao/vimicro-ai-mc:768_cpu bash${NCMD}
+${BOLD}
+2) Convert other AI models to Caffe models${NBOLD}
+  See how to convert other AI models to Caffe models:
+  ${CMD}pto2caffe --help${NCMD}
+${BOLD}
+3) Convert Caffe models to NPU models${NBOLD}
+  See how to convert Caffe models to NPU models:
+  ${CMD}caffe2npu --help${NCMD}
+"
