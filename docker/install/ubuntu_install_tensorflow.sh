f1f6c4f827505895caed4a2f6e854387b5e4b64c
duruyao <duruyao@gmail.com>
2023-07-24 16:50:43 +0800

    chore: add 'Dockerfile.cpu' for platform amd64 (cpu only)

---
+++ docker/install/ubuntu_install_tensorflow.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+set -e
+set -u
+set -o pipefail
+
+apt-install-and-clear -y --no-install-recommends libhdf5-dev
+
+pip3 install --no-cache-dir \
+  keras==2.9 \
+  tensorflow==2.9.3
