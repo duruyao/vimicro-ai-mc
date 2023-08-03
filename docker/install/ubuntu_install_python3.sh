6f8f14310335e9d19a306b8249e49a1c29575211
duruyao <duruyao@gmail.com>
2023-08-03 11:33:18 +0800

    feat: support for installing specific versions of python3 (3.7, 3.8, 3.9)

--- docker/install/ubuntu_install_python3.sh
+++ docker/install/ubuntu_install_python3.sh
@@ -9,6 +9,9 @@ apt-get update && apt-install-and-clear -y --no-install-recommends \
   software-properties-common
 
 PYTHON_VERSION="3.7"
+if [ "${1-}" == "3.8" ] || [ "${1-}" == "3.9" ]; then
+  PYTHON_VERSION="${1}"
+fi
 add-apt-repository -y ppa:deadsnakes/ppa
 
 apt-get update && apt-install-and-clear -y --no-install-recommends \
@@ -23,12 +26,13 @@ curl -ksSLO "https://bootstrap.pypa.io/get-pip.py" &&
   python"${PYTHON_VERSION}" get-pip.py &&
   rm -f get-pip.py
 
+update-alternatives --install /usr/bin/python python /usr/bin/python"${PYTHON_VERSION}" 1
 update-alternatives --install /usr/bin/python3 python3 /usr/bin/python"${PYTHON_VERSION}" 1
 #pushd /usr/bin &&
-#  ln -sf python3.7 /usr/bin/python3 &&
-#  ln -sf python3.7m /usr/bin/python3m &&
-#  ln -sf python3.7-config /usr/bin/python3-config &&
-#  ln -sf python3.7m-config /usr/bin/python3m-config &&
-#  popd
+#  ln -sf python"${PYTHON_VERSION}" /usr/bin/python3 &&
+#  ln -sf python"${PYTHON_VERSION}"m /usr/bin/python3m &&
+#  ln -sf python"${PYTHON_VERSION}"-config /usr/bin/python3-config &&
+#  ln -sf python"${PYTHON_VERSION}"m-config /usr/bin/python3m-config
+#popd
 
 pip3 config set global.no-cache-dir true
