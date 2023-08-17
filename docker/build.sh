746e59896da2687f1174756d5bf629ae2ce277ea
duruyao <duruyao@gmail.com>
2023-08-17 10:41:45 +0800

    feat: use 'ubuntu18.04' and 'nvidia/cuda:10.2-cudnn7-devel-ubuntu18.04' as basic images

--- docker/build.sh
+++ docker/build.sh
@@ -1,19 +1,22 @@
 #!/usr/bin/env bash
 
 set -x
+set -e
+set -u
+set -o pipefail
 
 # usage:
 # nohup bash build.sh >/dev/null 2>&1 &
-# tail -f nohup-cpu.out
 # tail -f nohup-gpu.out
+# tail -f nohup-cpu.out
 # tail -f nohup-cpu_arm.out
 
-docker buildx create --use --name insecure-builder --buildkitd-flags '--allow-insecure-entitlement security.insecure' || true
-
-docker buildx build --allow security.insecure --platform linux/amd64    -t duruyao/vimicro-mc:cpu     -f Dockerfile.cpu     --load . --progress plain >nohup-cpu.out     2>&1
-docker buildx build --allow security.insecure --platform linux/amd64    -t duruyao/vimicro-mc:gpu     -f Dockerfile.gpu     --load . --progress plain >nohup-gpu.out     2>&1
-docker buildx build --allow security.insecure --platform linux/arm64/v8 -t duruyao/vimicro-mc:cpu_arm -f Dockerfile.cpu_arm --load . --progress plain >nohup-cpu-arm.out 2>&1
-
-docker push duruyao/vimicro-mc:cpu
+docker build -t duruyao/vimicro-mc:gpu -f Dockerfile.gpu --load . --progress plain >nohup-gpu.out 2>&1
 docker push duruyao/vimicro-mc:gpu
+
+docker build -t duruyao/vimicro-mc:cpu -f Dockerfile.cpu --load . --progress plain >nohup-cpu.out 2>&1
+docker push duruyao/vimicro-mc:cpu
+
+docker buildx create --use --name insecure-builder --buildkitd-flags "--allow-insecure-entitlement security.insecure" || true
+docker buildx build --allow security.insecure --platform linux/arm64/v8 -t duruyao/vimicro-mc:cpu_arm -f Dockerfile.cpu_arm --load . --progress plain >nohup-cpu-arm.out 2>&1
 docker push duruyao/vimicro-mc:cpu_arm
