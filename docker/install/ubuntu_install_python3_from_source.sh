16fb1e7cc2d6e44789a7857b3c7b6be6ab4b2943
duruyao <duruyao@gmail.com>
2023-08-17 15:45:16 +0800

    feat: install 'python3.9.17' via 'ubuntu_install_python3_from_source.sh'

---
+++ docker/install/ubuntu_install_python3_from_source.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env bash
+
+set -x
+set -e
+set -u
+set -o pipefail
+
+PYTHON_VERSION="3.9.17"
+if [ "${1:-"DEFAULT_VALUE"}" != "DEFAULT_VALUE" ]; then
+  PYTHON_VERSION="${1}"
+fi
+
+apt-get update && apt-install-and-clear -y --no-install-recommends \
+  build-essential \
+  zlib1g-dev \
+  libncurses5-dev \
+  libgdbm-dev \
+  libnss3-dev \
+  libssl-dev \
+  libreadline-dev \
+  libffi-dev \
+  wget
+
+mkdir -p /install/ubuntu_install_python3_from_source
+pushd /install/ubuntu_install_python3_from_source
+
+wget "https://www.python.org/ftp/python/${PYTHON_VERSION}/Python-${PYTHON_VERSION}.tar.xz"
+tar -xf "Python-${PYTHON_VERSION}.tar.xz"
+pushd "Python-${PYTHON_VERSION}"
+./configure --enable-optimizations --prefix=/usr
+make -j $(($(nproc) - 1))
+make altinstall
+popd
+
+PYTHON_VERSION="$(echo "${PYTHON_VERSION}" | cut -d. -f1-2)"
+#python"${PYTHON_VERSION}" -c "$(curl -kfsSL "https://bootstrap.pypa.io/get-pip.py")"
+curl -ksSLO "https://bootstrap.pypa.io/get-pip.py" &&
+  python"${PYTHON_VERSION}" get-pip.py &&
+  rm -f get-pip.py
+
+update-alternatives --install /usr/bin/python python /usr/bin/python"${PYTHON_VERSION}" 1
+update-alternatives --install /usr/bin/python3 python3 /usr/bin/python"${PYTHON_VERSION}" 1
+#pushd /usr/bin &&
+#  ln -sf python"${PYTHON_VERSION}" /usr/bin/python3 &&
+#  ln -sf python"${PYTHON_VERSION}"m /usr/bin/python3m &&
+#  ln -sf python"${PYTHON_VERSION}"-config /usr/bin/python3-config &&
+#  ln -sf python"${PYTHON_VERSION}"m-config /usr/bin/python3m-config
+#popd
+
+pip3 config set global.no-cache-dir true
+
+popd
+rm -rf /install/ubuntu_install_python3_from_source
