c2eb4f3497d5cf553db5591ad8b8d901c38acd91
duruyao <duruyao@gmail.com>
2023-10-10 15:19:54 +0800

    feat: use ubuntu 22.04, cuda 11.8.0 and cudnn 8
    * build cpu image based on 'ubuntu:22.04'
    * build gpu image based on 'nvidia/cuda:11.8.0-cudnn8-devel-ubuntu22.04'

--- docker/build.sh
+++ docker/build.sh
@@ -12,11 +12,15 @@ set -o pipefail
 # tail -f nohup-cpu_arm.out
 
 docker build --build-arg BUILD_DATE="$(TZ=UTC-8 date "+%Y-%m-%dT%H:%M:%S+08:00")" -t duruyao/vimicro-mc:gpu -f Dockerfile.gpu --load . --progress plain >nohup-gpu.out 2>&1
+docker image tag duruyao/vimicro-mc:gpu duruyao/vimicro-mc:gpu-cuda11.8.0-cudnn8-ubuntu22.04
 docker push duruyao/vimicro-mc:gpu
+docker push duruyao/vimicro-mc:gpu-cuda11.8.0-cudnn8-ubuntu22.04
 
 docker build --build-arg BUILD_DATE="$(TZ=UTC-8 date "+%Y-%m-%dT%H:%M:%S+08:00")" -t duruyao/vimicro-mc:cpu -f Dockerfile.cpu --load . --progress plain >nohup-cpu.out 2>&1
+docker image tag duruyao/vimicro-mc:cpu duruyao/vimicro-mc:cpu-ubuntu22.04
 docker push duruyao/vimicro-mc:cpu
+docker push duruyao/vimicro-mc:cpu-ubuntu22.04
 
-docker buildx create --use --name insecure-builder --buildkitd-flags "--allow-insecure-entitlement security.insecure" || true
-docker buildx build --allow security.insecure --platform linux/arm64/v8 -t duruyao/vimicro-mc:cpu_arm -f Dockerfile.cpu_arm --load . --progress plain >nohup-cpu-arm.out 2>&1
-docker push duruyao/vimicro-mc:cpu_arm
+#docker buildx create --use --name insecure-builder --buildkitd-flags "--allow-insecure-entitlement security.insecure" || true
+#docker buildx build --allow security.insecure --platform linux/arm64/v8 -t duruyao/vimicro-mc:cpu_arm -f Dockerfile.cpu_arm --load . --progress plain >nohup-cpu-arm.out 2>&1
+#docker push duruyao/vimicro-mc:cpu_arm
--- docker/install/ubuntu_setup_tz.sh
+++ docker/install/ubuntu_setup_tz.sh
@@ -5,7 +5,7 @@ set -e
 set -u
 set -o pipefail
 
-export DEBIAN_FRONTEND=noninteractive
-export TZ=Etc/UTC
-ln -snf /usr/share/zoneinfo/${TZ} /etc/localtime
-echo ${TZ} >/etc/timezone
+export DEBIAN_FRONTEND="noninteractive"
+export TZ="Etc/UTC"
+ln -snf /usr/share/zoneinfo/"${TZ}" /etc/localtime
+echo "${TZ}" >/etc/timezone
